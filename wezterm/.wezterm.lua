6d937725abbe474833de19313389ec86081f53da
Einar <einargudnig@gmail.com>
2024-11-30 14:58:34 +0000

    aerospace configs, very basic

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,20 +3,14 @@ local wezterm = require("wezterm")
 return {
 	-- color_scheme = 'termnial.sexy',
 	color_scheme = "Catppuccin Mocha",
-  default_cursor_style = 'BlinkingBar',
+	default_cursor_style = "BlinkingBar",
 	enable_tab_bar = false,
 	font_size = 16.0,
-	font = wezterm.font("JetBrains Mono"),
+	-- font = wezterm.font("JetBrains Mono"),
+	font = wezterm.font("Geist Mono"),
 	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
 
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
 	window_background_opacity = 1.0,
 	-- window_background_opacity = 0.78,
 	-- window_background_opacity = 0.20,
