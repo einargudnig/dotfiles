6a0b372f457d43e264779f552dcc7fa91cf03739
Einar <einargudnig@gmail.com>
2025-02-01 09:40:37 +0000

    new folke picker

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,9 +1,9 @@
 return {
   -- I like the file tree on the right side!
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = { position = "right" },
-    },
-  },
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   opts = {
+  --     window = { position = "right" },
+  --   },
+  -- },
 }
